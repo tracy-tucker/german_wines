4cb693c598153e0e24de6cbce9fb911a341cc377
treuther <tracyreuther@gmail.com>
2020-06-24 04:32:29 -0500

    start method additions for basic user interaction

--- lib/german_wines/cli.rb
+++ lib/german_wines/cli.rb
@@ -1,5 +1,19 @@
 class GermanWines::CLI
+    #Top-level note: any time you have a different action, make it a different method
+    
+    # start method should be the main flow that calls different methods
     def start
-        puts "Hello!"
+        puts "Welcome to the German Wine App!"
+        puts "Here are the top German wines that we have for you today:"
+        # make a call to the Scraper class - scrape all the wines
+        # list all the wines
+        list_wines
+        # ask for input
+        # call another method
+    end
+
+    def list_wines
+        GermanWines::Wine.all.each.with_index(1) do |wine, index|
+            puts "#{index}. #{wine.name}"
     end
 end
