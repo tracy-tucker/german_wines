003232f2d26e5bc4da85ee33aa151ebfe39ae622
treuther <tracyreuther@gmail.com>
2020-06-24 04:39:13 -0500

    corrections to envrionment file and description class addition

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,11 @@
 require "nokogiri"
+require "pry"
+require "open-uri"
 
 require_relative "german_wines/version"
 require_relative "german_wines/cli"
+require_relative "german_wines/wine.rb"
+require_relative "german_wines/description.rb"
 
 # module GermanWines
 #   class Error < StandardError; end
---
+++ lib/german_wines/description.rb
@@ -0,0 +1,2 @@
+class GermanWines::Description
+end
