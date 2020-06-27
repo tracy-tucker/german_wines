4ba527f83a6abd7c446080fe6294b90cd1debbca
treuther <tracyreuther@gmail.com>
2020-06-27 16:48:29 -0500

    corrections to scraper. additions to wine.rb - added self method and key/value pair

--- lib/german_wines/scraper.rb
+++ lib/german_wines/scraper.rb
@@ -6,14 +6,16 @@ class GermanWines::Scraper
         array_of_wines = index_page.css("#itemsBlock div.product-item")
 
         array_of_wines.each do |wine_card|
-            (
-                name: wine_card.css("name").text,
+            attributes = {
+                name: wine_card.css(".name").text,
                 url: wine_card.css("a")[0]["href"],
                 sub_title: wine_card.css(".product-subtitle").text,
                 price: wine_card.css("span.price").text,
                 list_price: wine_card.css(".retailprice").text,
                 savings: wine_card.css(".savings").text
-            )
+            }
+            wine = GermanWines::Wine.new(attributes)
+            binding.pry
         end
     end
 
--- lib/german_wines/wine.rb
+++ lib/german_wines/wine.rb
@@ -1,2 +1,9 @@
 class GermanWines::Wine
+    attr_accessor :name, :url, :sub_title, :price, :list_price, :savings
+
+    def initialize(att_hash)
+        att_hash.each do |key, value|
+            self.send("#{key}=", value)
+        end
+    end
 end
