61a00474acd962235076a9c9d19be09c51171805
treuther <tracyreuther@gmail.com>
2020-06-29 18:01:22 -0500

    addition want_more_info to give user more options after selection

--- lib/german_wines/cli.rb
+++ lib/german_wines/cli.rb
@@ -13,7 +13,7 @@ class GermanWines::CLI
         sort_wines
         # list all the wines
         list_wines
-        # ask for user input
+        # ask for user input and show a teaser
         get_wine
         # call another method
     end
@@ -62,5 +62,22 @@ class GermanWines::CLI
         puts "- #{wine.sale}"
         puts "- #{wine.list_price}"
         puts "- #{wine.savings}"
+        # ask user if they want more info
+        want_more_info(wine)
+        puts "Please select a corresponding movie number, or type 'exit' to exit."
+    end
+
+    def want_more_info(wine)
+        puts "Read more about this wine? (Y/N)"
+        input = "nil"
+        until input == "Y" || input == "N"
+            input = gets.strip.upcase
+            puts "type Y(yes) or N(no)"
+        end
+        if input == "y"
+            #2nd scrape
+        else
+            puts "you ended"
+        end
     end
 end
